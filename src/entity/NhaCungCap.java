ed2ada3cb433e276ddffa282a1e7594767bc3f0b
Tuan2002T <truongtuan2422@gmail.com>
2022-10-10 20:34:18 +0700

    entity cua Tuan

--- src/entity/NhaCungCap.java
+++ src/entity/NhaCungCap.java
@@ -1,5 +1,53 @@
 package entity;
 
 public class NhaCungCap {
-
+	private String maNCC;
+	private String tenNCC;
+	private String nguoiDaiDien;
+	private String SĐT;
+	private String diaChi;
+	
+	public NhaCungCap(String maNCC, String tenNCC, String nguoiDaiDien, String sĐT, String diaChi) {
+		super();
+		this.maNCC = maNCC;
+		this.tenNCC = tenNCC;
+		this.nguoiDaiDien = nguoiDaiDien;
+		SĐT = sĐT;
+		this.diaChi = diaChi;
+	}
+	public String getMaNCC() {
+		return maNCC;
+	}
+	public void setMaNCC(String maNCC) {
+		this.maNCC = maNCC;
+	}
+	public String getTenNCC() {
+		return tenNCC;
+	}
+	public void setTenNCC(String tenNCC) {
+		this.tenNCC = tenNCC;
+	}
+	public String getNguoiDaiDien() {
+		return nguoiDaiDien;
+	}
+	public void setNguoiDaiDien(String nguoiDaiDien) {
+		this.nguoiDaiDien = nguoiDaiDien;
+	}
+	public String getSĐT() {
+		return SĐT;
+	}
+	public void setSĐT(String sĐT) {
+		SĐT = sĐT;
+	}
+	public String getDiaChi() {
+		return diaChi;
+	}
+	public void setDiaChi(String diaChi) {
+		this.diaChi = diaChi;
+	}
+	@Override
+	public String toString() {
+		return "NhaCungCap [maNCC=" + maNCC + ", tenNCC=" + tenNCC + ", nguoiDaiDien=" + nguoiDaiDien + ", SĐT=" + SĐT
+				+ ", diaChi=" + diaChi + "]";
+	}
 }
--- src/entity/NhaXuatBan.java
+++ src/entity/NhaXuatBan.java
@@ -1,5 +1,33 @@
 package entity;
 
 public class NhaXuatBan {
+	private String maNXB;
+	private String tenNXB;
+	
+	
+	public NhaXuatBan(String maNXB, String tenNXB) {
+		super();
+		this.maNXB = maNXB;
+		this.tenNXB = tenNXB;
+	}
+	
+	public String getMaNXB() {
+		return maNXB;
+	}
+	public void setMaNXB(String maNXB) {
+		this.maNXB = maNXB;
+	}
+	public String getTenNXB() {
+		return tenNXB;
+	}
+	public void setTenNXB(String tenNXB) {
+		this.tenNXB = tenNXB;
+	}
 
+	@Override
+	public String toString() {
+		return "NhaXuatBan [maNXB=" + maNXB + ", tenNXB=" + tenNXB + "]";
+	}
+	
+	
 }
