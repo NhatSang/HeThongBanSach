645654a443e1e0f6a0d7ec754b9d5aa83f9a3ecf
NhatSang <sangnn8@gmail.com>
2022-10-10 23:46:44 +0700

    fix GD va them lib, icon

--- src/ui/App.java
+++ src/ui/App.java
@@ -1,7 +1,14 @@
 package ui;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class App {
 	public static void main(String[] args) {
+	try {
+		UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+	} catch (Exception e) {
+	}
 	GD_DangNhap f = new GD_DangNhap();
 	f.setVisible(true);
 }
